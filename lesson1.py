1242edb20d5375da69c24f39d4231c3d3c56ba79
ChiVy <chivy@gmail.com>
2026-01-07 17:50:54 +0700

    Add python

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains 
+
+# String 
+first_name = "Vỹ"
+print (f"Tên mình là {first_name}")
